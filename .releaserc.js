9a3a62dc22de0b489a4ba48979509a7e7018c901
VaibhavJain97 <45284347+VaibhavJain97@users.noreply.github.com>
2023-08-04 01:57:53 +0530

    Update .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["origin/main"], // Change this to the name of your default branch
+  branches: ["origin/release/release"], // Change this to the name of your default branch
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
