f3e2fdb22e4b71a6d0016cba061081868183f8fc
Vaibhav <vaibhavjain.nitj@gmail.com>
2023-08-03 23:51:55 +0530

    fix: added the configurations files

---
+++ .releaserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: ["testing-commits"], // Change this to the name of your default branch
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    "@semantic-release/git",
+  ],
+};
