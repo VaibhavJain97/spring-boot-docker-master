82bc7064a133fa806f2d1a5d4ac2db1291a14f70
VaibhavJain97 <45284347+VaibhavJain97@users.noreply.github.com>
2023-08-04 03:20:44 +0530

    breaking: adding a new authentication feature block
    breaking: adding a new authentication feature block

--- .releaserc.js
+++ .releaserc.js
@@ -7,4 +7,13 @@ module.exports = {
     "@semantic-release/github",
     "@semantic-release/git",
   ],
+  // Specify the commit types that are allowed
+  preset: 'conventionalcommits',
+  presetConfig: {
+    types: [
+      { type: 'breaking', section: 'Breaking Changes' },
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+    ],
+  },
 };
